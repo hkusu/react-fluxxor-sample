7538c7cd8c28bcd9ae1238c21e244c2f8742bbe4
Hiroyuki Kusu <kusu0806@gmail.com>
2015-06-23 17:28:38 +0900

    optimize the task of gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,33 @@
 var gulp = require('gulp');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
-var webserver = require('gulp-webserver');
+var browserSync = require('browser-sync');
+var shell = require('gulp-shell');
 
-gulp.task('browserify', function() {
-  browserify('./app.jsx', { debug: true })
-    .transform(babelify)
-    .bundle()
-    .on("error", function (err) { console.log("Error : " + err.message); })
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./'))
+gulp.task('build:js', shell.task(
+  'npm run build'
+));
+
+gulp.task('build:html', function() {
+  gulp.src('./src/*.html')
+    .pipe(gulp.dest('./dest/'))
 });
 
+gulp.task('browser-sync:run', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./dest/",
+      index: "index.html"
+    }
+  });
+});
+
+gulp.task('browser-sync:reload', function() {
+  browserSync.reload();
+});
+
+gulp.task('build', ['build:html', 'build:js']);
 gulp.task('watch', function() {
-  gulp.watch('./*.jsx', ['browserify'])
+  gulp.watch('./src/*.jsx', ['build:js']);
+  gulp.watch('./src/*.html', ['build:html']);
+  gulp.watch('./dest/*', ['browser-sync:reload']);
 });
-
-gulp.task('webserver', function() {
-  gulp.src('./')
-    .pipe(webserver({
-      host: '127.0.0.1',
-      livereload: true
-    })
-  );
-});
-
-gulp.task('default', ['browserify', 'watch', 'webserver']);
+gulp.task('serve', ['watch', 'browser-sync:run']);
